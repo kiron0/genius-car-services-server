2f1aaba5b92d0021ae7865251c4aed1f6c9abe72
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-22 21:39:55 +0600

    changes something

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express();
 require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
+const ObjectId = require("mongodb").ObjectId;
 
 // Middleware
 app.use(cors());
@@ -16,11 +17,44 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-client.connect(async (err) => {
-  const usersCollection = client.db("geniusCar").collection("service");
-  console.log('Genius Car Service is running on MongoDB...');
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+
+    app.get("/service", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // POST
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const result = await serviceCollection.deleteOne(query);
+      res.send(result);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("Genius Car Services");
