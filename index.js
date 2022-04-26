0b2bdf3a746a754de3a90a1f68ec87a30551262f
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-26 20:00:48 +0600

    orders

--- index.js
+++ index.js
@@ -53,6 +53,14 @@ async function run() {
     });
 
     // Order Collection API
+
+    app.get("/order", async (req, res) => {
+      const query = {};
+      const cursor = orderCollection.find(query);
+      const orders = await cursor.toArray();
+      res.send(orders);
+    });
+
     app.post("/order", async (req, res) => {
       const order = req.body;
       const result = await orderCollection.insertOne(order);
