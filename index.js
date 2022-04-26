c0bc75147af8835e608b36ecf045128e24851b54
Toufiq Hasan Kiron <toufiqhasankiron2@gmail.com>
2022-04-26 20:11:47 +0600

    orders

--- index.js
+++ index.js
@@ -55,7 +55,8 @@ async function run() {
     // Order Collection API
 
     app.get("/orders", async (req, res) => {
-      const query = {};
+      const email = req.query.email;
+      const query = {email: email};
       const cursor = orderCollection.find(query);
       const orders = await cursor.toArray();
       res.send(orders);
